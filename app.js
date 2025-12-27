40513ce49413270adf8180bfa36bcac65fe53f8d
Your Name <your-email@example.com>
2025-12-27 23:16:56 +0530

    Move JavaScript to HTML, remove try-catch from signup/signin, display both username and password

--- app.js
+++ app.js
@@ -56,14 +56,27 @@ app.post("/signin", (req, res) => {
 });
 
 function auth(req, res, next) {
-    const token = req.headers.token;
-    const decodedData = jwt.verify(token, JWT_SECRET);
-    if (decodedData.username) {
-        req.username = decodedData.username;
-        next();
-    } else {
-        res.status(401).json ({
-            message: "Unauthorized"
+    try {
+        const token = req.headers.token;
+
+        if(!token) {
+            return res.status(401).json({
+                message: "Token missing"
+            });
+        }
+        const decodedData = jwt.verify(token, JWT_SECRET);
+
+        if (decodedData.username) {
+            req.username = decodedData.username;
+            next();
+        } else {
+            res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+    }catch {
+        res.status(401).json({
+            message: "Invalid or expired token"
         });
     }
 }
