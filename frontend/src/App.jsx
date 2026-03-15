544ded9a03df827ef3859d7ced8ece9f7c61ddde
haideralyy01 <haideralyy01@gmail.com>
2026-03-15 10:56:29 +0000

    react frontend intialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import LoginPage from "./components/Login"
+import SigninPage from "./components/Signup"
+
+function App() {
+
+  return (
+    <>
+      <SigninPage />
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
